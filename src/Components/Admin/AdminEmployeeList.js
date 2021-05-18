5499b95e25f7ea403bc1587d8bcff9909bce81a3
adityamulik <aditya.mulik@gmail.com>
2021-05-18 13:52:46 -0400

    added material table with edit, delete crud operations in Admin Panel

--- src/Components/Admin/AdminEmployeeList.js
+++ src/Components/Admin/AdminEmployeeList.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import useEmployeeRecords from '../CustomHooks/useEmployeeRecords';
-import ReactVirtualizedTable from '../Table/EmployeeTableAdmin';
-import {Link} from 'react-router-dom';
+import EmployeeTableAdmin from '../Table/EmployeeTableAdmin';
+import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 
 const AdminEmployeeList = () => {
 
-  <h1>PayPay Employee Admin Dashboard</h1>
-  const { employees, isLoading: isLoadingEmployees, setEmployees } = useEmployeeRecords();
+  const useStyles = makeStyles((theme) => ({
+    fab: {
+      margin: theme.spacing(2),
+    },
+    absolute: {
+      position: 'absolute',
+      bottom: theme.spacing(2),
+      right: theme.spacing(3),
+    },
+  }));
 
+  const classes = useStyles();
+  
   return (
     <div className="employeeList">
       <h1>PayPay Admin Employee Dashboard</h1>
       <br></br>
-      <Link
-        to="/admin/createemployee"
-      >
-      Add Employee
-      </Link>
-      <p>Employee's with Performance Review Completed! Click the name to view details.</p>
-      <br></br>
-      <ReactVirtualizedTable 
-        employees={employees}
+      <Tooltip title="Add Employee" aria-label="add">
+        <Fab color="primary" className={classes.fab} href="/admin/createemployee">
+          <AddIcon>
+          </AddIcon>
+        </Fab>
+      </Tooltip>      
+      <EmployeeTableAdmin
       />
     </div>
   )
